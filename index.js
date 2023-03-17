2c1a83da65582de0bab656d9230553ab6a9c2238
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2023-03-17 15:52:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const Post = require('./models/Post');
+
+// Config
+// Template Engine
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Config
+// Body Parser - Não Funciona mais
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Routes
+app.get('/reg', (req, res) => {
+  res.render('forms');
+});
+
+app.post('/add', (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content
+  }).then(() => {
+    res.redirect('/');
+  }).catch((err) => {
+    res.send(`There was an error ${err}`);
+  });
+});
+
+app.get('/', (req, res) => {
+  Post.findAll().then((posts) => {
+    res.render('home', {posts: posts});
+  });
+});
+
+app.get('/delete/:id', (req, res) => {
+  Post.destroy({where: {id: req.params.id}}).then(() => {
+    res.send('Successfully deleted Post!');
+  }).catch(err => {
+    res.send('This post do not exist.');
+  });
+});
+
+app.listen(3030, () => {
+  console.log('SERVER RUNNING ON URL: http://localhost:3030');
+});
